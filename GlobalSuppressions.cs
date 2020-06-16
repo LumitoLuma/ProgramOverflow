0ca5019fc9b2efb379d380ef9c5be678fc8da40a
LumitoLuma <lumito@protonmail.com>
2020-06-16 17:26:32 +0200

    Update GlobalSuppressions.cs

--- GlobalSuppressions.cs
+++ GlobalSuppressions.cs
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 // This file is used by Code Analysis to maintain SuppressMessage
-=======
 ﻿// This file is used by Code Analysis to maintain SuppressMessage
->>>>>>> c8ad49fb890ef50e6609fe8dcd6d2a19507e0eb7
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
@@ -11,8 +8,5 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<pendiente>", Scope = "member", Target = "~M:ProgramOverflow.Form1.Form1_Load(System.Object,System.EventArgs)")]
 [assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<pendiente>", Scope = "member", Target = "~M:ProgramOverflow.Form1.Button2_Click(System.Object,System.EventArgs)")]
-<<<<<<< HEAD
 [assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<pendiente>", Scope = "member", Target = "~M:ProgramOverflow.Form1.Button1_Click(System.Object,System.EventArgs)")]
-=======
 [assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<pendiente>", Scope = "member", Target = "~M:ProgramOverflow.Form1.Button1_Click(System.Object,System.EventArgs)")]
->>>>>>> c8ad49fb890ef50e6609fe8dcd6d2a19507e0eb7
