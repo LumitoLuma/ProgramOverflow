c9cdf5ad574efa2624fb0f4c009dba251212a737
Lumito <50250351+LumitoLuma@users.noreply.github.com>
2020-07-09 22:35:45 +0200

    Reverted changes from last commit

--- Form1.cs
+++ Form1.cs
@@ -12,7 +12,7 @@ using System.Windows.Forms;
 
 namespace ProgramOverflow
 {
-    public class Form1 : Form
+    public partial class Form1 : Form
     {
         byte counterRipPc;
         byte counterRunPrograms;
